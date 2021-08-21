bfecc58773de2696a5d0c8175427e005f0451d3c
Tiagodev <tiagodev@protonmail.com>
2021-08-21 13:08:51 -0700

    add modal material ui

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,6 @@
-import { Fab, makeStyles, Tooltip } from "@material-ui/core";
+import { Container, Fab, makeStyles, Modal, Tooltip } from "@material-ui/core";
 import { Add as AddIcon } from "@material-ui/icons";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
   fab: {
@@ -7,17 +8,36 @@ const useStyles = makeStyles((theme) => ({
     bottom: 20,
     right: 20,
   },
+  container: {
+    width: 500,
+    height: 550,
+    backgroundColor: "white",
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    margin: "auto",
+    [theme.breakpoints.down("sm")]: {
+      width: "100vw",
+      height: "100vh",
+    },
+  },
 }));
 
 const Add = () => {
+  const [open, setOpen] = useState(false);
   const classes = useStyles();
   return (
     <>
-      <Tooltip title="Add" aria-label="add">
+      <Tooltip title="Add" aria-label="add" onClick={() => setOpen(true)}>
         <Fab color="primary" className={classes.fab}>
           <AddIcon />
         </Fab>
       </Tooltip>
+      <Modal open={open}>
+        <Container className={classes.container}>My modal</Container>
+      </Modal>
     </>
   );
 };

7ecfa45452f2861f2a03e2760884ba6c7c8251fe
Tiagodev <tiagodev@protonmail.com>
2021-08-23 16:18:57 -0700

    add using Snackbar from material-ui

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,4 +1,19 @@
-import { Container, Fab, makeStyles, Modal, Tooltip } from "@material-ui/core";
+import {
+  Container,
+  Fab,
+  makeStyles,
+  Modal,
+  Tooltip,
+  TextField,
+  MenuItem,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  FormLabel,
+  Button,
+  Snackbar,
+} from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
 import { Add as AddIcon } from "@material-ui/icons";
 import { useState } from "react";
 
@@ -23,11 +38,26 @@ const useStyles = makeStyles((theme) => ({
       height: "100vh",
     },
   },
+  form: {
+    padding: theme.spacing(2),
+  },
+  item: {
+    marginBottom: theme.spacing(3),
+  },
 }));
+
+const Alert = (props) => <MuiAlert elevation={6} variant="filled" {...props} />;
 
 const Add = () => {
   const [open, setOpen] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
   const classes = useStyles();
+
+  const handleClose = (e, reason) => {
+    if (reason === "clickaway") return;
+    setOpenAlert(false);
+  };
+
   return (
     <>
       <Tooltip title="Add" aria-label="add" onClick={() => setOpen(true)}>
@@ -36,8 +66,89 @@ const Add = () => {
         </Fab>
       </Tooltip>
       <Modal open={open}>
-        <Container className={classes.container}>My modal</Container>
+        <Container className={classes.container}>
+          <form className={classes.form} autocomplete="off">
+            <div className={classes.item}>
+              <TextField
+                id="standard-basic"
+                label="Title"
+                size="small"
+                style={{ width: "100%" }}
+              />
+              <TextField
+                id="outlined-multiline-static"
+                multiline
+                rows={4}
+                defaultValue="Tell your story..."
+                variant="outlined"
+                label="Description"
+                size="small"
+                style={{ width: "100%", marginTop: "20px" }}
+              />
+            </div>
+            <div className={classes.item}>
+              <TextField select label="Visibility" value="Public">
+                <MenuItem value="Public">Public</MenuItem>
+                <MenuItem value="Private">Private</MenuItem>
+                <MenuItem value="Unlisted">Unlisted</MenuItem>
+              </TextField>
+            </div>
+            <div className={classes.item}>
+              <FormLabel component="legend">Who can comment?</FormLabel>
+              <RadioGroup>
+                <FormControlLabel
+                  value="everybody"
+                  control={<Radio size="small" />}
+                  label="Everybody"
+                />
+                <FormControlLabel
+                  value="My friends"
+                  control={<Radio size="small" />}
+                  label="My friends"
+                />
+                <FormControlLabel
+                  value="Nobody"
+                  control={<Radio size="small" />}
+                  label="Nobody"
+                />
+                <FormControlLabel
+                  value="Custom"
+                  disabled
+                  control={<Radio size="small" />}
+                  label="Custom (Premium)"
+                />
+              </RadioGroup>
+            </div>
+            <div className={classes.item}>
+              <Button
+                variant="outlined"
+                color="primary"
+                style={{ marginRight: "20px" }}
+                onClick={() => setOpenAlert(true)}
+              >
+                Create
+              </Button>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={() => setOpen(false)}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
+        </Container>
       </Modal>
+      <Snackbar
+        open={openAlert}
+        autoHideDuration={2000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+      >
+        <Alert onClose={handleClose} severity="success">
+          This is a success message!
+        </Alert>
+      </Snackbar>
     </>
   );
 };
